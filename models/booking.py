af65e9a5572d68d7291623a181eb226491352b50
Mary Fagan <m5fgn@outlook.com>
2021-04-03 00:52:25 +0100

    Adding Classes

---
+++ models/booking.py
@@ -0,0 +1,6 @@
+class Booking:
+
+    def __init__(self, booking_id, member_id, session_id, attended):
+        self.booking_id = booking_id
+        self.member_id = member_id
+        self.session_id = session_id
---
+++ models/member.py
@@ -0,0 +1,8 @@
+class Member:
+    
+    def __init__(self, member_id, first_name, last_name, member_type, status):
+        self.member_id = member_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.member_type = member_type
+        self.status = status
