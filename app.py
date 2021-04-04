f59fe1ccc7df696b5f8b47163600630fce393408
Mary Fagan <m5fgn@outlook.com>
2021-04-04 08:54:11 +0100

    Adding Logo and Nav Bar to Base

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from controllers import booking_controller
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', title='Home')
 
 if __name__ == '__main__':
     app.run(debug=True)
