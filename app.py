5625623d634851bebbd68d56736455e7df73c744
Mary Fagan <m5fgn@outlook.com>
2021-04-04 17:57:10 +0100

    Add Initial Blueprints and Nav Links

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask, render_template
 
+from controllers.member_controller import members_blueprint
+from controllers.gymsession_controller import gymsessions_blueprint
+# from controllers.booking_controller import booking_blueprint
+
 app = Flask(__name__)
 
-from controllers import member_controller
-from controllers import gymsession_controller
-from controllers import booking_controller
+app.register_blueprint(members_blueprint)
+app.register_blueprint(gymsessions_blueprint)
+# app.register_blueprint(booking_blueprint)
 
 @app.route('/')
 def home():
     return render_template('index.html', title='Home')
+
+@app.route('/help')
+def help():
+    return render_template('help.html', title='Home')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ controllers/gymsession_controller.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+from flask import Blueprint
+from models.gymsession import Gymsession
+import repositories.gymsession_repository as gymsession_repository
+
+gymsessions_blueprint = Blueprint('gymsessions', __name__)
+
+@gymsessions_blueprint.route('/gymsessions')
+def gymsessions():
+    return render_template('gymsessions/index.html', title='Sessions')
