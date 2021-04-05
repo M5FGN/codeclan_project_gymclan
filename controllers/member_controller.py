ce975e659fd859249d2614ecf48224814afa8692
Mary Fagan <m5fgn@outlook.com>
2021-04-05 17:36:48 +0100

    Adding add member route

--- controllers/member_controller.py
+++ controllers/member_controller.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from flask import Blueprint
 from models.member import Member
 import repositories.member_repository as member_repository
@@ -20,6 +20,16 @@ def view(id):
 def add():
     return render_template('members/add.html', title='Add New Member')
 
+@members_blueprint.route('/members/add_member', methods=['POST'])
+def add_member():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    member_type = request.form['member_type']
+    member_status = request.form['member_status']
+    member = Member(first_name, last_name, member_type, member_status)
+    member_repository.save(member)
+    return redirect('/members')
+
 @members_blueprint.route('/members/edit')
 def edit():
     return render_template('members/edit.html', title='Edit Member')
@@ -33,3 +43,4 @@ def delete_member(id):
 def delete_member_save(id):
     member_repository.delete_member(id)
     return redirect('/members')
+
